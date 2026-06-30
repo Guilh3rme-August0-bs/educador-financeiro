e371bc0b6c47e587a8b39308231488e87a61f42e
Guilherme <gsilva12401321@gmail.com>
2026-06-30 10:54:02 -0300

    criação de questões para o formulário, navegação entre as questões, máscara monetária e validação dos inputs

--- src/components/features/simulation/Form.tsx
+++ src/components/features/simulation/Form.tsx
@@ -1,17 +1,39 @@
 import { Progress } from "./Progress"
 import { FormStep } from "./FormStep"
-import { PiggyBank } from "lucide-react"
+import { simulationFormSteps } from "../../../data/simulation"
+import { useState } from "react"
 
 export const SimulationForm = () => {
+
+    //Estado para navegar entre as etapas
+    const [currentStepIndex, setCurrentIndex] = useState(0)
+    const totalSteps = simulationFormSteps.length
+    const currentStep = simulationFormSteps[currentStepIndex]
+
+    //avançar formulário
+    const nextStep = () => {
+        if (currentStepIndex + 1 > totalSteps - 1) {
+            return
+        }
+
+        setCurrentIndex((prev) => prev + 1)
+    }
+
+    //voltar ao passo anterior
+    const previousStep = () => {
+        if (currentStepIndex === 0) {
+            return
+        }
+        setCurrentIndex((prev) => prev - 1)
+    }
+
+    //formulário
     return (
         <div className="mb-8 text-center">
-            <Progress currentStep={6} totalSteps={10} />
-            <FormStep
-                icon={PiggyBank}
-                title="Renda Mensal Bruta"
-                question="Somando todas as fontes de renda, qual é o valor depositado na sua conta todo mês?"
-                inputProps={{ type: 'text', placeholder: 'ex: 5.000,00', prefix: 'R$', suffix: 'meses'}}
-                />
+            <Progress currentStep={currentStepIndex + 1} totalSteps={totalSteps} />
+            <FormStep key={currentStep.id} {...currentStep} 
+            onBack={previousStep} onNext={nextStep}
+            hideBackButton={currentStepIndex === 0}/>
         </div>
     )
 }
--- src/components/features/simulation/FormStep.tsx
+++ src/components/features/simulation/FormStep.tsx
@@ -2,8 +2,11 @@ import type { LucideIcon } from "lucide-react"
 import { Button } from "../../shared/Button"
 import { Input, type InputProps } from "../../shared/Input"
 import { ArrowLeft, ArrowRight } from "lucide-react"
+import { useState, type SyntheticEvent } from "react"
+import { formatCurrencyMask } from "../../../utils/currency"
 
-interface FormStepProps {
+export interface FormStepProps {
+    id: string
     icon: LucideIcon
     title: string
     question: string
@@ -14,7 +17,31 @@ interface FormStepProps {
     }
 }
 
-export const FormStep = ({ icon: Icon, title, question, inputProps, submitButtonProps }: FormStepProps) => {
+interface ActionsButtonsProps {
+    onBack: () => void
+    onNext: () => void
+    hideBackButton?: boolean
+}
+
+export const FormStep = ({
+    icon: Icon,
+    title,
+    question,
+    inputProps,
+    submitButtonProps,
+    onBack,
+    onNext,
+    hideBackButton }: FormStepProps & ActionsButtonsProps) => {
+
+    //estado para obter valor do input
+    const [inputValue, setInputValue] = useState('')
+
+    //função para impedir o recarregamento da página ao navegar pelas etapas
+    const handleSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        onNext()
+    }
+
     return (
         <div className="bg-card rounded-lg p-6 shadow-[4px_4px_18px_0px_rgba(0,0,0,0.2)] sm:p-8 text-left">
             <div className="bg-primary mb-4 flex h-15 w-15 items-center justify-center rounded-lg">
@@ -23,24 +50,35 @@ export const FormStep = ({ icon: Icon, title, question, inputProps, submitButton
             <h2 className="text-primary mb-1 text-xs font-semibold tracking-widest uppercase">
                 {title}</h2>
             <h3 className="text-foreground mb-6 text-x1 leading-snug font-semibold sm:text-2x1">{question}</h3>
-            <form className="flex flex-col gap-4">
-                <Input {...inputProps} />
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                <Input {...inputProps} value={inputValue} 
+                onChange={(e) => setInputValue(inputProps.prefix === 'R$'
+                ? formatCurrencyMask(e.target.value) 
+                : e.target.value)} />
                 <div className="flex flex-col gap-3 sm:flex-row sm:gap-6">
-                <Button
-                    icon={ArrowLeft}
-                    type="button"
-                    variant="ghost"
-                    className="order 2 flex-1 justify-center rounded-lg py-3 sm:order-1">
-                    Anterior
-                </Button>
-                <Button
-                    icon={!submitButtonProps ? ArrowRight : undefined}
-                    type="button"
-                    variant="primary"
-                    className="order 2 flex-1 justify-center rounded-lg py-3 sm:order-2">
-                    {submitButtonProps ?.label ?? 'Próximo'}
-                    {submitButtonProps ?.emojiIcon}
-                </Button>
+
+                    {/* botão só será renderizado se hideBackButton for false */}
+
+                    {!hideBackButton && (
+                        <Button
+                            icon={ArrowLeft}
+                            type="button"
+                            onClick={onBack}
+                            variant="ghost"
+                            className="order 2 flex-1 justify-center rounded-lg py-3 sm:order-1">
+                            Anterior
+                        </Button>
+                    )}
+                    <Button
+                        icon={!submitButtonProps ? ArrowRight : undefined}
+                        type="button"
+                        onClick={onNext}
+                        disabled={!inputValue}
+                        variant="primary"
+                        className="order 2 flex-1 justify-center rounded-lg py-3 sm:order-2">
+                        {submitButtonProps?.label ?? 'Próximo'}
+                        {submitButtonProps?.emojiIcon}
+                    </Button>
                 </div>
             </form>
         </div>
